5d4e070bf8563aad8ef1fff717669f9e07e3e97e
Heydon <heydon@heydonworks.com>
2014-10-26 17:19:05 +0000

    new sounds and route change a11y

--- client/devolepor2015.js
+++ client/devolepor2015.js
@@ -1,7 +1,7 @@
   Meteor.startup(function () {
 
     // Initialize sessions
-    Session.set('skills', ['Node', 'HTML5', 'SVG', 'MongoDB', 'PHP', 'RWD', 'OOCSS', 'WAI-ARIA', 'Sass', 'AngularJS', 'Meteor', 'Microdata', 'Git', 'Vim']);
+    Session.set('skills', ['Node', 'HTML5', 'SVG', 'PHP', 'RWD', 'OOCSS', 'WAI-ARIA', 'Sass', 'AngularJS', 'Meteor', 'Microdata', 'Git']);
     Session.set('chosenSkills', []);
 
   });
--- client/helpers+events.js
+++ client/helpers+events.js
@@ -8,10 +8,6 @@ Handlebars.registerHelper('percent', function(key) {
 
 /* 'start' */
 
-Template.start.rendered = function() {
-	console.log(soundStart);
-};
-
 Template.start.events({
 	'click button': function() {
 		var soundStart = new buzz.sound('/sounds/start.wav');
@@ -32,14 +28,51 @@ Template.skills.events({
 	'change [type="checkbox"]': function(e) {
 		var chosen = Session.get('chosenSkills');
 		if ($(e.currentTarget).is(':checked')) {
-			chosen.push($(e.currentTarget).val());
-			Session.set('chosenSkills', chosen);
-			var soundSelect = new buzz.sound('/sounds/select.wav');
-			soundSelect.play({volume: 50});
+			var soundSlideup = new buzz.sound('/sounds/slideup.wav');
+			soundSlideup.play({volume: 50});
+			setTimeout(function () {
+				chosen.push($(e.currentTarget).val());
+				Session.set('chosenSkills', chosen);
+			}, 750);
 		} else {
-			Session.set('chosenSkills', _.without(chosen, $(e.currentTarget).val()));
 			var soundError = new buzz.sound('/sounds/error.wav');
 			soundError.play({volume: 50});
+			setTimeout(function () {
+				Session.set('chosenSkills', _.without(chosen, $(e.currentTarget).val()));
+			}, 750);
 		}
+	},
+	'click .continue': function(e) {
+		if (Session.get('chosenSkills').length === Session.get('skills').length) {
+			Session.set('coronersReport', 'You forgot to save time for sleepin\'.<br> You have gone mad and died.');
+			Router.go('/ded');
+		} else {
+			var omitted = _.difference(Session.get('skills'), Session.get('chosenSkills'));
+			var keyskill = _.sample(omitted);
+			Session.set('coronersReport', 'Someone asked you a question about '+keyskill+' that you couldn\'t answer.<br> You have died of shame.');
+			Router.go('/ded');
+		}
+		Session.set('chosenSkills', []);
 	}
 });
+
+/* /ded */
+
+Template.ded.helpers({
+	report: function() {
+		return Session.get('coronersReport');
+	}
+});
+
+Template.ded.events({
+	'click button': function() {
+		Router.go('/');
+	}
+});
+
+
+
+Template.ded.rendered = function() {
+	var soundDed = new buzz.sound('/sounds/ded.wav');
+	soundDed.play({volume: 50});
+};
